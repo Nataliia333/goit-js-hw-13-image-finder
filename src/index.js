87d079e0ac701c547353fdc42fd747772293058b
Nataliia333 <n.o.prykhodko@gmail.com>
2021-01-23 19:48:12 +0200

    поиск и просмотр изображений по ключевому слову

--- src/index.js
+++ src/index.js
@@ -2,37 +2,68 @@ import './styles.css';
 // import fetchCard from './js/apiService.js';
 import cardTpl from './templates/card.hbs';
 import refs from './js/refs';
-import ApiService from './js/apiService.js';
+import apiService from './js/apiService.js';
 // import debounce from 'lodash.debounce';
-
-
-const apiService = new ApiService();
 
 refs.searchForm.addEventListener('submit', onSearch);
   
-function onSearch(e) {
-    e.preventDefault();
+function onSearch(event) {
+    event.preventDefault();
 
-    // const form = event.currentTarget;
-    apiService.query = e.currentTarget.elements.query.value;
+    const form = event.currentTarget;
+    apiService.query = form.elements.query.value;
 
     apiService.resetPage();
-    fetchCard();
-    // clearCardList();
+    fetchCardImage();
+    clearCardList();
     
 }
 
-function fetchCard() {
-apiService.fetchCard().then(data=> {
-   cardsMarkup(data);
+function fetchCardImage() {
+apiService.fetchCard().then(hits=> {
+   cardsMarkup(hits);
 // console.log(data)
    
    });
 }
       
-function cardsMarkup(data) {
-refs.cardList.insertAdjacentHTML('beforeend', cardTpl(data));
+function cardsMarkup(hits) {
+refs.cardList.insertAdjacentHTML('beforeend', cardTpl(hits));
 }
+      
+function clearCardList() {
+refs.cardList.innerHTML = '';
+}
+
+// ************
+
+// const apiService = new ApiService();
+
+// refs.searchForm.addEventListener('submit', onSearch);
+  
+// function onSearch(e) {
+//     e.preventDefault();
+
+//     // const form = event.currentTarget;
+//     apiService.query = e.currentTarget.elements.query.value;
+
+//     apiService.resetPage();
+//     fetchCardImage();
+//     clearCardList();
+    
+// }
+
+// function fetchCardImage() {
+// apiService.fetchCard().then(hits=> {
+//    cardsMarkup(hits);
+// // console.log(data)
+   
+//    });
+// }
+      
+// function cardsMarkup(hits) {
+// refs.cardList.insertAdjacentHTML('beforeend', cardTpl(hits));
+// }
       
 // function clearCardList() {
 // refs.cardList.innerHTML = '';
