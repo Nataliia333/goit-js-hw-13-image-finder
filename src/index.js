3e914b49bc2192bd0d3a0d64597de8857394b594
Nataliia333 <n.o.prykhodko@gmail.com>
2021-01-23 22:55:04 +0200

    правки

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import './styles.css';
-// import fetchCard from './js/apiService.js';
 import cardTpl from './templates/card.hbs';
 import refs from './js/refs';
 import apiService from './js/apiService.js';
 // import debounce from 'lodash.debounce';
-
-// const loadMoreBtn = new LoadMoreBtn({
-//    selector: 'button[data-action="load-more"]',
-//    hidden: true,
-//  });
 
 refs.searchForm.addEventListener('submit', onSearch);
   
@@ -17,18 +11,16 @@ function onSearch(event) {
 
     const form = event.currentTarget;
     apiService.query = form.elements.query.value;
+    refs.cardList.innerHTML = '';
 
     apiService.resetPage();
     fetchCardImage();
-    clearCardList();
-    form.reset();
-    
+    form.reset();    
 }
 
 function fetchCardImage() {
 apiService.fetchCard().then(hits=> {
    cardsMarkup(hits);
-// console.log(data)
  });
 }
       
@@ -36,46 +28,13 @@ function cardsMarkup(hits) {
 refs.cardList.insertAdjacentHTML('beforeend', cardTpl(hits));
 }
       
-function clearCardList() {
-refs.cardList.innerHTML = '';
-}
-
 refs.loadMoreBtn.addEventListener('click', () => {
    apiService.fetchCard().then(hits=> {
       cardsMarkup(hits);
+      
+   window.scrollTo({
+      top: document.documentElement.offsetHeight,
+      behavior: 'smooth',
+   });
 });
 });
-
-// ************
-
-// const apiService = new ApiService();
-
-// refs.searchForm.addEventListener('submit', onSearch);
-  
-// function onSearch(e) {
-//     e.preventDefault();
-
-//     // const form = event.currentTarget;
-//     apiService.query = e.currentTarget.elements.query.value;
-
-//     apiService.resetPage();
-//     fetchCardImage();
-//     clearCardList();
-    
-// }
-
-// function fetchCardImage() {
-// apiService.fetchCard().then(hits=> {
-//    cardsMarkup(hits);
-// // console.log(data)
-   
-//    });
-// }
-      
-// function cardsMarkup(hits) {
-// refs.cardList.insertAdjacentHTML('beforeend', cardTpl(hits));
-// }
-      
-// function clearCardList() {
-// refs.cardList.innerHTML = '';
-// }
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -32,38 +32,3 @@ export default  {
    this.searchQuery = value;
  },
 }
-
-// export default class ApiService {
-//    constructor() {
-//       this.searchQuery = '';
-//       this.page = 1;
-//    }
-
-//    fetchCard() {
-    
-//     const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${apiKey}`;
-//     return fetch(url)
-//       .then(res => res.json())
-//       .then(({ hits }) => {
-//          this.incrementPage();
-//          return { hits };
-//        });
-//    };
-
-//  incrementPage() {
-//    this.page += 1;
-//  }
-
-//  resetPage() {
-//    this.page = 1;
-//  }
-
-//  get query() {
-//    return this.searchQuery;
-//  }
-
-//  set query(newQuery) {
-//    this.searchQuery = newQuery;
-//  }
-// }
- 
